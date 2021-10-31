cc17c3f7b5ba0944512fa4720d7208b8e9067fa3
Alex Gibson <argibson02@gmail.com>
2021-10-31 11:45:01 -0700

    Added in some basic get routes for testing connections

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+const { Article, User } = require('../models');
 const withAuth = require('../utils/auth.js');
 
-// GET all galleries for homepage
+// GET all articles for homepage
 router.get('/', async (req, res) => {
   try {
-    const dbGalleryData = await Gallery.findAll({
+    const dbArticleData = await Article.findAll({
       include: [
         {
-          model: Painting,
-          attributes: ['filename', 'description'],
+          model: User,
+          attributes: ['id', 'username'],
         },
       ],
     });
 
-    const galleries = dbGalleryData.map((gallery) => // change to articles
-      gallery.get({ plain: true })
+    const articles = dbArticleData.map((article) => // change to articles
+      article.get({ plain: true })
     );
 
     res.render('homepage', {
-      galleries,
+      articles,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
@@ -28,49 +28,98 @@ router.get('/', async (req, res) => {
   }
 });
 
-// GET one gallery
-router.get('/gallery/:id', async (req, res) => {
+// GET one article
+router.get('/article/:id', async (req, res) => {
   // If the user is not logged in, redirect the user to the login page
   withAuth(req, res);
 
-    // If the user is logged in, allow them to view the gallery
-    try {
-      const dbGalleryData = await Gallery.findByPk(req.params.id, {
-        include: [
-          {
-            model: Painting,
-            attributes: [
-              'id',
-              'title',
-              'artist',
-              'exhibition_date',
-              'filename',
-              'description',
-            ],
-          },
-        ],
-      });
-      const gallery = dbGalleryData.get({ plain: true });
-      res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-});
-
-// GET one painting
-router.get('/painting/:id', withAuth, async (req, res) => {
+  // If the user is logged in, allow them to view the article
   try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
-
-    const painting = dbPaintingData.get({ plain: true });
-
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+    const dbArticleData = await Article.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'description',
+            'author',
+            'timestamp',
+          ],
+        },
+        {
+          model: User,
+          attributes: ['id', 'username'],
+        },
+      ],
+    });
+    const article = dbArticleData.get({ plain: true });
+    res.render('view-single-article', { article, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+
+
+// GET one comment  DELETE AFTER TESTING
+router.get('/article/test/:id', withAuth, async (req, res) => {
+  try {
+    const dbArticleData = await Article.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'description',
+            'author',
+            'timestamp',
+          ],
+        },
+        {
+          model: User,
+          attributes: ['id', 'username'],
+        },
+      ],
+    });
+    const articleData = dbArticleData.get({ plain: true });    
+    res.status(200).json(articleData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// GET one comment  DELETE AFTER TESTING
+router.get('/comment/test/:id', withAuth, async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findByPk(req.params.id);
+
+    const commentData = dbCommentData.get({ plain: true });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one USER  DELETE AFTER TESTING
+router.get('/user/test/:id', withAuth, async (req, res) => {
+  try {
+    const dbUserData = await User.findByPk(req.params.id);
+
+    const userData = dbUserData.get({ plain: true });
+
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
