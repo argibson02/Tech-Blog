490795e6c29d4d8e89828dd7eadf68ea6c724211
Alex Gibson <argibson02@gmail.com>
2021-11-01 21:37:27 -0700

    Cleaned up CSS styling all over handlebars files. Added some quality-of-life {unless} handlebars logic for differing view between loggedin vs logged out users.

--- seeds/articleData.js
+++ seeds/articleData.js
@@ -2,14 +2,14 @@ const { Article } = require('../models');
 
 const articleData = [
   {
-    article_title: 'This is a Test Article',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    article_title: 'How now, Horatio! you tremble and look pale: Is not this something more than fantasy?',
+    description: '\'Tis sweet and commendable in your nature, Hamlet, To give these mourning duties to your father: But, you must know, your father lost a father. That father lost, lost his, and the survivor bound In filial obligation for some term To do obsequious sorrow: but to persever In obstinate condolement is a course Of impious stubbornness. \'tis unmanly grief.',
     author: 'Person1',  // might be able to swap this out for username from USER
     user_id: 1
   },
   {
-    article_title: 'This is a Second Test Article',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    article_title: 'When he the ambitious Norway combated, So frowned he once, when, in an angry parle, He smote the sledded Polacks on the ice. \'Tis strange.',
+    description: 'Though yet of Hamlet our dear brother\'s death The memory be green, and that it us befitted To bear our hearts in grief and our whole kingdom To be contracted in one brow of woe, Yet so far hath discretion fought with nature That we with wisest sorrow think on him, Together with remembrance of ourselves. Therefore our sometime sister, now our queen, The imperial jointress to this warlike state, Have we, as \'twere with a defeated joy,-- With an auspicious and a dropping eye, With mirth in funeral and with dirge in marriage, In equal scale weighing delight and dole,-- Taken to wife: nor have we herein barred Your better wisdoms, which have freely gone With this affair along. For all, our thanks. Now follows, that you know, young Fortinbras, Holding a weak supposal of our worth, Or thinking by our late dear brother\'s death Our state to be disjoint and out of frame, Colleagued with the dream of his advantage, He hath not failed to pester us with message, Importing the surrender of those lands Lost by his father, with all bonds of law, To our most valiant brother. So much for him. Now for ourself and for this time of meeting: Thus much the business is: we have here writ To Norway, uncle of young Fortinbras,-- Who, impotent and bed-rid, scarcely hears Of this his nephew\'s purpose,--to suppress His further gait herein, in that the levies, The lists and full proportions, are all made Out of his subject: and we here dispatch You, good Cornelius, and you, Voltimand, For bearers of this greeting to old Norway, Giving to you no further personal power To business with the king, more than the scope Of these delated articles allow. Farewell, and let your haste commend your duty.',
     author: 'Person2',  // might be able to swap this out for username from USER
     user_id: 2
   },
--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,25 +2,25 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    description: 'Blossoming Apricot 1',
+    description: 'In what particular thought to work I know not, But in the gross and scope of my opinion, This bodes some strange eruption to our state.',
     author: 'Person2',
     article_id: 1,
     user_id: 2
   },
   {
-    description: 'Blossoming Apricot 2',
+    description: 'I think it be no other but een so: Well may it sort that this portentous figure Comes armed through our watch so like the king That was and is the question of these wars.',
     author: 'Person1',
     article_id: 1,
     user_id: 1
   },
   {
-    description: 'Blossoming Apricot 3',
+    description: 'That can I At least, the whisper goes so. Our last king, Whose image even but now appeard to us, Was, as you know, by Fortinbras of Norway, Thereto prickd on by a most emulate pride, Dared to the combat in which our valiant Hamlet-- For so this side of our known world esteemd him-- Did slay this Fortinbras who by a seald compact,',
     author: 'Person2',
     article_id: 1,
     user_id: 2
   },
   {
-    description: 'Blossoming Apricot 4',
+    description: 'A mote it is to trouble the minds eye. In the most high and palmy state of Rome, A little ere the mightiest Julius fell, The graves stood tenantless and the sheeted dead Did squeak and gibber in the Roman streets: As stars with trains of fire and dews of blood,',
     author: 'Person1',
     article_id: 2,
     user_id: 1
